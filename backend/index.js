8fbb050150f2213ab98894a19f787c5cda1f9470
Kunal <kunal.rohitas29@gmail.com>
2022-01-04 22:12:15 +0530

    did some minor changes

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 const connectToMongo = require('./db')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 const port = 3000;
 
+app.use(morgan('dev'))
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.status(200).send("hello world")
 })
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
     console.log(`listening at  http://localhost:${port}`);
--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const NotesSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: String,
         required: true
@@ -20,4 +24,4 @@ const NotesSchema = new Schema({
     }
 });
 
-modeule.exports = mongoose.model('notes', NotesSchema);
+module.exports = mongoose.model('notes', NotesSchema);
